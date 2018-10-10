1ea3da4154976218910d3e1f3aaa00eb64f42a7e
George de Borba Nardes <6473091@univali.br>
2018-10-10 12:24:30 -0300

    Versão melhorada

--- main.cpp
+++ main.cpp
@@ -16,11 +16,11 @@ int indice (int n, int tam) {
     return (n % tam);
 }
 
-void printar (unordered_map<int, bloco> cache[],int qtd_memoria, int tam) {
+void printar (unordered_map<int, bloco> cache[],int qtd_vias, int qtd_bloco) {
     system("cls");
-    for (int i = 0; i < qtd_memoria; i++){
+    for (int i = 0; i < qtd_vias; i++){
         cout << "Bloco\tValor\tValidade" << endl;
-        for (int j = 0; j < tam; j++){
+        for (int j = 0; j < qtd_bloco; j++){
             printf("%2d   \t", j);
             if ( cache[i][j].val == true)
                 printf("%4d\t", cache[i][j].tag);
@@ -34,45 +34,58 @@ void printar (unordered_map<int, bloco> cache[],int qtd_memoria, int tam) {
 
 int main()
 {
-    int tam;
-    int qtd_memoria;
+    int qtd_bloco;
+    int qtd_vias;
+    int custo_bit = 0;
     cout << "Quantos blocos tem a sua memoria: ";
-    cin >> tam;
-    cout << "Quantas memorias voce tem: ";
-    cin >> qtd_memoria;
+    cin >> qtd_bloco;
+    cout << "Quantas vias voce tem: ";
+    cin >> qtd_vias;
 
-    unordered_map<int, bloco> cache[qtd_memoria];
+
+    unordered_map<int, bloco> cache[qtd_vias];
+
+    for ( int i = 0; i < qtd_vias; i++)
+        for ( int j = 0; j < qtd_bloco; j++)
+            cache[i][j].tempo = 0;
+
     int endereco;
-    int tempo = 0;
-    system("cls");
-    do {
-        printar (cache, qtd_memoria, tam);
+    int tempo = 1;
 
+    system("cls");
+
+
+    do {
+        printar (cache, qtd_vias, qtd_bloco);
         cout << "Endereco para procurar:";
         cin >> endereco;
         int mais_antigo = 99999;
         int memo_to_change;
         bool encontrado = false;
-        for ( int i = 0; i < qtd_memoria; i++){
-            if ( mais_antigo > cache[i][indice(endereco, tam)].tempo ){
-                mais_antigo =cache[i][indice(endereco, tam)].tempo;
+
+        for ( int i = 0; i < qtd_vias; i++){
+            if ( mais_antigo > cache[i][indice(endereco, qtd_bloco)].tempo ){
+                mais_antigo = cache[i][indice(endereco, qtd_bloco)].tempo;
                 memo_to_change = i;
             }
-            if ( endereco == cache[i][indice(endereco, tam)].tag ) {
-                if ( cache[i][indice(endereco, tam)].val == true ){
-                    cache[i][indice(endereco, tam)].tempo = tempo;
+            if ( endereco == cache[i][indice(endereco, qtd_bloco)].tag ) {
+
+                if ( cache[i][indice(endereco, qtd_bloco)].val == true ){
+                    cache[i][indice(endereco, qtd_bloco)].tempo = tempo;
                     cout << "Acerto\n";
                     encontrado = true;
                 }
-                else
+                else {
                     cout << "Vencido\n";
+                    cache[i][indice(endereco, qtd_bloco)].val = true;
+                }
             }
         }
         if ( encontrado == false ) {
             cout << "O endereco nao foi encontrado\n";
-            cache[memo_to_change][indice(endereco, tam)].tag = endereco;
-            cache[memo_to_change][indice(endereco, tam)].tempo = tempo;
-            cache[memo_to_change][indice(endereco, tam)].val = true;
+            cache[memo_to_change][indice(endereco, qtd_bloco)].tag = endereco;
+            cache[memo_to_change][indice(endereco, qtd_bloco)].tempo = tempo;
+            cache[memo_to_change][indice(endereco, qtd_bloco)].val = true;
         }
         Sleep (750);
         tempo++;
